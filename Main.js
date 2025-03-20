c068c0e9f3d6d7b3897ec401c1de625b2a6bc1b5
hanakkristof <146178797+hanakkristof@users.noreply.github.com>
2025-03-20 10:59:06 +0100

    Add files via upload
    No soul just boring ahh templates :(((

---
+++ Main.js
@@ -0,0 +1,9 @@
+const url = "https://raw.githubusercontent.com/hanakkristof/datajson/refs/heads/main/data.json"
+
+getData(url, renderProjects)
+
+function renderProjects(data){
+  data.(array).forEach(element => {
+    
+  });
+}
