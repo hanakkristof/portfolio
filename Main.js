0c1d7a4f93715985f536d96c27d48f4bd7bee35d
hanakkristof <146178797+hanakkristof@users.noreply.github.com>
2025-03-26 10:51:10 +0100

    Deleted unnecessary files

--- Main.js
+++
@@ -1,9 +0,0 @@
-const url = "https://raw.githubusercontent.com/hanakkristof/datajson/refs/heads/main/data.json"
-
-getData(url, renderProjects)
-
-function renderProjects(data){
-  data.(array).forEach(element => {
-    
-  });
-}
